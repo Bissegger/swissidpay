d2d0f070f2f2bb7799609ac8693fed974f5e3559
Bissegger <bisseggerkobi@gmail.com>
2026-03-27 13:35:33 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,17 @@
+// stärker verschleiert
+const _secure = ["Rzd2JDJwUSB4UjkjYkw4d1RmJjNrWnMx"];
+
+function decode(x){
+  return atob(x);
+}
+
+function login(){
+  const input = document.getElementById("pw").value;
+
+  if(input === decode(_secure[0])){
+    document.getElementById("loginBox").style.display="none";
+    document.getElementById("app").style.display="block";
+  } else {
+    alert("❌ Zugriff verweigert");
+  }
+}
