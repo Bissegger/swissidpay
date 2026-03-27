3458c549ea6c7711705509f67f390fb5b921f0fc
Bissegger <bisseggerkobi@gmail.com>
2026-03-27 14:03:33 +0100

    Create package.js

---
+++ swissnote-ultra/backend/package.js
@@ -0,0 +1,12 @@
+{
+  "name": "swissnote",
+  "version": "1.0.0",
+  "main": "server.js",
+  "dependencies": {
+    "express": "^4.18.2",
+    "jsonwebtoken": "^9.0.0",
+    "bcrypt": "^5.1.0",
+    "mongoose": "^7.0.0",
+    "cors": "^2.8.5"
+  }
+}
